35158aaca79be962d980a4c07e973db128c56632
Vyacheslav Shashkov <spase74@gmail.com>
2019-10-24 00:36:56 +0300

    Initial support for HX1230

--- src/altimeter/display.h
+++ src/altimeter/display.h
@@ -17,7 +17,28 @@ U8G2_PCD8544_84X48_1_4W_HW_SPI u8g2(U8G2_R2, SS, DC_PIN, U8X8_PIN_NONE);
 #define DISPLAY_WIDTH 96
 #define DISPLAY_HEIGHT 68
 #define DISPLAY_LINES_IN_MENU 6
-U8G2_HX1230_96X68_1_4W_HW_SPI u8g2(U8G2_R0, SS, U8X8_PIN_NONE, U8X8_PIN_NONE);
+
+// U8G2_HX1230_96X68_1_3W_SW_SPI u8g2(U8G2_R0, SS, U8X8_PIN_NONE);
+
+// Comment it to use 3-wire hardware SPI emulation
+//#define DC_PIN PIN_DC
+
+extern "C" uint8_t u8x8_byte_arduino_hw_spi_3w(u8x8_t *u8g2, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+
+class U8G2_HX1230_96X68_1_HW_SPI : public U8G2 {
+    public:
+    U8G2_HX1230_96X68_1_HW_SPI(const u8g2_cb_t *rotation, uint8_t cs, uint8_t dc = U8X8_PIN_NONE, uint8_t reset = U8X8_PIN_NONE) : U8G2() {
+#ifdef DC_PIN        
+        u8g2_Setup_hx1230_96x68_1(&u8g2, rotation, u8x8_byte_arduino_hw_spi, u8x8_gpio_and_delay_arduino);
+#else
+        u8g2_Setup_hx1230_96x68_1(&u8g2, rotation, u8x8_byte_arduino_hw_spi_3w, u8x8_gpio_and_delay_arduino);
+#endif
+        u8x8_SetPin_4Wire_HW_SPI(getU8x8(), cs, dc, reset);
+    }
+};
+
+U8G2_HX1230_96X68_1_HW_SPI u8g2(U8G2_R0, SS, PIN_DC);
+
 #endif
 #endif
 #endif
