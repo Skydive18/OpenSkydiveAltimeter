30500badd9c2f39d7621ecc042f245d6eac3c07a
Vyacheslav Shashkov <spase74@gmail.com>
2020-03-18 12:04:52 +0300

    Fix keyboard

--- src/altimeter/keyboard.h
+++ src/altimeter/keyboard.h
@@ -3,11 +3,11 @@
 #include "Arduino.h"
 #include "hwconfig.h"
 
-#define BTN1_PRESSED (!(PORTF & 0x10))
+#define BTN1_PRESSED (!(PINF & 0x10))
 #define BTN1_RELEASED !BTN1_PRESSED
-#define BTN2_PRESSED (!(PORTD & 0x40))
+#define BTN2_PRESSED (!(PINF & 0x40))
 #define BTN2_RELEASED !BTN2_PRESSED
-#define BTN3_PRESSED (!(PORTF & 0x20))
+#define BTN3_PRESSED (!(PINF & 0x20))
 #define BTN3_RELEASED !BTN3_PRESSED
 
 uint8_t getKeypress(uint16_t timeout = 20000); // timeout in 15ms chunks, default is 5 min
