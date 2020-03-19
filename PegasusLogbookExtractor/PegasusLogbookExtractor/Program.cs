0a0ca95f4490af2377bb80d5cc9fc1ea177f0b35
Vyacheslav Shashkov <spase74@gmail.com>
2020-03-19 23:45:55 +0300

    Added sinple log extractor

---
+++ PegasusLogbookExtractor/PegasusLogbookExtractor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PegasusLogbookExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serialPort = new SerialPort();
+            serialPort.PortName = "COM18";
+            serialPort.Open();
+            // Toggle DTR
+
+            serialPort.RtsEnable = true;
+
+            System.Threading.Thread.Sleep(1000);
+
+            serialPort.DtrEnable = true; // 'DTR -
+            System.Threading.Thread.Sleep(50);
+
+            serialPort.DtrEnable = false; // 'DTR +
+            serialPort.RtsEnable = false;
+
+            StringBuilder sb = new StringBuilder();
+            string data = string.Empty;
+
+            for (int i = 0; i < 10; ++i)
+            {
+                serialPort.Write("?");
+                Thread.Sleep(200);
+            }
+
+            do
+            {
+                data = serialPort.ReadLine();
+                sb.Append(data + "\n");
+            } while (data != "PEGASUS_END");
+            string result = sb.ToString();
+
+            var outputFile = File.CreateText("pegasus.raw");
+            outputFile.WriteLine(result);
+            outputFile.Close();
+        }
+    }
+}
